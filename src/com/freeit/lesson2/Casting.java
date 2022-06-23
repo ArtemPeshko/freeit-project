35fed60c2eae73fe289e9431c17e19ccc7613202
Artem Peshko <artem.peshko@alseda.by>
2022-06-23 22:23:30 +0300

    our classwork

---
+++ src/com/freeit/lesson2/Casting.java
@@ -0,0 +1,20 @@
+package com.freeit.lesson2;
+
+/**
+ * Created by Artem Peshko on 23.06.2022
+ * E-Mail artem.peshko@alseda.by
+ * E-Mail artem.peshko@gmail.com
+ */
+public class Casting {
+    public static void main(String[] args) {
+        byte b = (byte) 127;
+        short s = 5;
+        long l = 5L;
+        float f = 0.5f;
+        double d = 0.5;
+
+        int a = 127;
+
+        byte bt = (byte) a;
+    }
+}
---
+++ src/com/freeit/lesson2/IfElseSwitchConstructors.java
@@ -0,0 +1,68 @@
+package com.freeit.lesson2;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * Created by Artem Peshko on 23.06.2022
+ * E-Mail artem.peshko@alseda.by
+ * E-Mail artem.peshko@gmail.com
+ */
+public class IfElseSwitchConstructors {
+    // if(true){}, if(){}else{}, if(){}else if(){}else{}
+
+    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Введите ваше имя: ");
+//        String name = scanner.nextLine();
+//        System.out.println("Введите день числом");
+//        int day = scanner.nextInt();
+//        System.out.println(isMale(name));
+//        System.out.println("Вы выбрали день: " + dayOfWeek(day));
+        sayGoodBay();
+    }
+
+    private static String isMale(String name) {
+        String[] letters = {"а", "е", "ё", "и", "у", "ы", "э", "о", "ю", "я"};
+        String letter = name.substring(name.length() - 1);
+        if (Arrays.asList(letters).contains(letter)) {
+            return "Женщина";
+        } else {
+            return "Мужчина";
+        }
+    }
+
+    private static void sayGoodBay() {
+        System.out.println("До встречи");
+    }
+
+    private static String dayOfWeek(int day) {
+        switch (day) {
+            case 1 -> {
+                return "Понедельник";
+            }
+            case 2 -> {
+                return "Вторник";
+            }
+            case 3 -> {
+                return "Среда";
+            }
+            case 4 -> {
+                return "Четверг";
+            }
+            case 5 -> {
+                return "Пятница";
+            }
+            case 6 -> {
+                return "Суббота";
+            }
+            case 7 -> {
+                return "Воскресенье";
+            }
+            default -> {
+                return "Нет такого дня";
+            }
+        }
+    }
+}
