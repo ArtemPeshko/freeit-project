7a30be5f26acbfd4594fecf80ac58e092603f715
Artem Peshko <artem.peshko@alseda.by>
2022-06-25 12:18:11 +0300

    our classwork

---
+++ src/com/freeit/lesson3/Cycles.java
@@ -0,0 +1,73 @@
+package com.freeit.lesson3;
+
+/**
+ * Created by Artem Peshko on 25.06.2022
+ * E-Mail artem.peshko@alseda.by
+ * E-Mail artem.peshko@gmail.com
+ */
+public class Cycles {
+    static int i;
+
+    public static void main(String[] args) throws InterruptedException {
+        //int i = 0; - начало нашего цикла
+        //i < 10; - условие до которого наш цикл работает
+        //i++
+
+        for (i = 0; i < 10; i++) {
+            System.out.println("Hello");
+        }
+//        for (int i = 0; i < 5; i++) {
+//            System.out.println("Hello from i");
+//            for (int j = 0; j < 5; j++) {
+//                System.out.println("Hello from j");
+//            }
+//        }
+//
+//        for (int i = 1; i < 100; i++) {
+//            if (i % 13 != 0) {
+//                continue;
+//            }
+//            System.out.println("i " + i);
+//        }
+//
+//        for (int i = 1; i < 100; i++) {
+//            if (i % 13 == 0) {
+//                break;
+//            }
+//            System.out.println("i " + i);
+//        }
+
+//        int i = 0;
+//        boolean isValid = false;
+//
+//        while (isValid) {
+//            System.out.println("Hello " + i);
+//            isValid = i++ < 5;
+//        }
+//
+//        // do-while
+//
+//        do {
+//            System.out.println("Hello " + i);
+//            Thread.sleep(1000);
+//            isValid = i++ < 5;
+//        } while (isValid);
+
+        // ?:
+//        int age = 18;
+//
+//        String name = age >= 18 ? "совершеннолетний" : "несовершеннолетний";
+//        System.out.println(name);
+
+//        int[] names = {1, 2, 3, 4};
+
+//        for (String name : names) {
+//            System.out.println(name);
+//        }
+
+//        for (int name : names) {
+//            System.out.println(name);
+//        }
+//
+    }
+}
