63909ed0a820d19d20bbdfbb71a1ef624b51af5e
Artem Peshko <artem.peshko@alseda.by>
2022-08-29 09:32:10 +0300

    our classwork

---
+++ src/com/freeit/lesson17/exceptions/ZeroException.java
@@ -0,0 +1,12 @@
+package com.freeit.lesson17.exceptions;
+
+public class ZeroException extends Exception {
+
+	public ZeroException() {
+	}
+
+	public ZeroException(String message) {
+		super(message);
+	}
+}
+
